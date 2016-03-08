58f29d3dd91f6981d3657d95dc46459f390957a7
Shaun <skushi3@gmail.com>
2016-03-08 06:12:21 -1000

    Added test cases for junit.

---
+++ src/iCalendarTest.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import org.junit.Test;
+
+
+public class iCalendarTest {
+
+  @Test
+  public void testGetStartTime() {
+      Events temp = new Events();
+      String year, month, day, time, datetime;
+      time = "120000";
+      year = "2016";
+      month = "03";
+      day = "11";
+      datetime = year + month + day;
+      temp.setDTStart(datetime + 'T' + time);
+      assertEquals("20160311T120000", temp.getDTStart());
+  }
+  
+  @Test
+  public void testGetEndTime() {
+      Events temp = new Events();
+      String year, month, day, time, datetime;
+      time = "130000";
+      year = "2016";
+      month = "03";
+      day = "11";
+      datetime = year + month + day;
+      temp.setDTEnd(datetime + 'T' + time);
+      assertEquals("20160311T130000", temp.getDTEnd());
+  }
+  
+  @Test
+  public void testGeoLocation() {
+      Events temp = new Events();
+      String geo = "84.5657;134.53435";
+      temp.setGeo(geo);
+      assertEquals("Geographic locations do not match", "84.5657;134.53435", temp.getGeo());
+  }
+  
+
+  
+
+
+}
